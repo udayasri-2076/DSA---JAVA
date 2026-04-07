fde3a46c0a3eb8e14587253af06739b89f888755
Udayasri <udayas1220@gmail.com>
2026-04-07 22:51:21 +0530

    Add files via upload

---
+++ P01_firstprogram.java
@@ -0,0 +1,15 @@
+/*Latest Java 
+void main() {
+    System.out.println("Hello World")
+}
+*/
+import java.util.*;
+
+public class P01_firstprogram { // Changed '1_firstprogram' to 'FirstProgram'
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello world" + " Hello dunia");
+        System.out.println("4 + 3");
+    }
+}
+
